1cdb37034512d2cb4c9e6b6bf45db25e8bc9d2f5
Adil-choudan <adilchoudan889@gmail.com>
2024-03-21 18:36:49 +0000

    222

--- src/Bstore/Pages/Shop.jsx
+++ src/Bstore/Pages/Shop.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Hero from '../Compenent/Heror/Hero';
+import Popular from '../Compenent/popular/Popular';
+import Offers from '../Compenent/Offers/Offers';
+import Newcollections from '../Compenent/Newcollections/Newcollections';
 
 const Shop = () => {
     return (
         <div>
             <Hero/>
+            <Popular/>
+            <Offers/>
+            <Newcollections/>
         </div>
     );
 }
